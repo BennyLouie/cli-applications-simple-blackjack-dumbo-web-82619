bb4e8ca728f6e224f6aba95a197715bb8f52ccb3
Benny Louie <bennylouie1412@gmail.com>
2019-08-15 23:59:13 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,8 +56,8 @@ end
 def runner
   welcome
   card_input = initial_round
-  display_card_total(card_input)
   card_total = hit?(card_input)
+  display_card_total(card_input)
   until card_total > 21 do
     if card_total > 21
       end_game(card_total)
